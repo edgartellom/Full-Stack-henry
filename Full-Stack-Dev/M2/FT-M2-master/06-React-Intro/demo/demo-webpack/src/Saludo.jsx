f2223beaca105b3c60fa824ef16d5ba179683174
edgartellom <edgar.tello@tecsup.edu.pe>
2022-09-25 19:36:05 -0300

    demo 06 React-Intro

--- Full-Stack-Dev/M2/FT-M2-master/06-React-Intro/demo/demo-webpack/src/Saludo.jsx
+++ Full-Stack-Dev/M2/FT-M2-master/06-React-Intro/demo/demo-webpack/src/Saludo.jsx
@@ -4,7 +4,7 @@ export default class Saludo extends React.Component {
   render() {
     const saludo = this.props.lang === 'es' ? 'Hola' : 'Hello';
     return (
-      <h1>{saludo}! {this.props.nombre}</h1>
+      <h1>{saludo} {this.props.nombre}!</h1>
     )
   };
 };
--- Full-Stack-Dev/M2/FT-M2-master/06-React-Intro/demo/demo-webpack/src/SaludoFuncional.jsx
+++ Full-Stack-Dev/M2/FT-M2-master/06-React-Intro/demo/demo-webpack/src/SaludoFuncional.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 export default function SaludoFuncional(props) {
   const saludo = props.lang === 'es' ? 'Hola' : 'Hello'
   return (
-    <h1>{saludo}! {props.nombre}</h1>
+    <h1>{saludo} {props.nombre}!</h1>
   )
 };
