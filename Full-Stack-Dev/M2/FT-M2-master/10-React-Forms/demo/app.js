2c00f34db4bbe52afafbfcba14ee7f3ecc464d47
edgartellom <edgar.tello@tecsup.edu.pe>
2022-10-10 17:01:53 -0300

    demo 10

--- Full-Stack-Dev/M2/FT-M2-master/10-React-Forms/demo/app.js
+++ Full-Stack-Dev/M2/FT-M2-master/10-React-Forms/demo/app.js
@@ -5,7 +5,19 @@ import Uncontrolled from './src/components/Uncontrolled.jsx';
 import Ejemplo from './src/components/Ejemplo.jsx';
 import DynamicInputs from './src/components/DynamicInputs.jsx';
 
-render(<div>
+render(
+  <div>
+    <h2>Controlled</h2>
+    <Form />
+    <hr />
+    <h2>Dinamic Inputs</h2>
+    <DynamicInputs />
+    <hr />
     <h2>Ejemplo</h2>
     <Ejemplo lang='hun'/>
-  </div>, document.getElementById('app'));
+    <hr />
+    <h2>Uncontrolled</h2>
+    <Uncontrolled />
+    <hr />
+  </div>, 
+  document.getElementById('app'));
